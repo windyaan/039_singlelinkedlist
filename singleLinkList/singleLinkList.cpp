94f9b4ff663bbcda5e83a2f8cd8477940a22e4ca
windyaan <windyanurmaretha@gmail.com>
2023-05-18 21:54:41 +0700

    Method addNode

--- singleLinkList/singleLinkList.cpp
+++ singleLinkList/singleLinkList.cpp
@@ -34,3 +34,14 @@ void addNode() {
 
 	Node* previous = START;
 	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
