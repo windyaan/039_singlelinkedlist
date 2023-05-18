f7a83d07a453ec7e8492f679f1847bb2f2f13a6b
windyaan <windyanurmaretha@gmail.com>
2023-05-18 22:07:15 +0700

    Program utama untuk menjalankan data (menampilkan teks)

--- singleLinkList/singleLinkList.cpp
+++ singleLinkList/singleLinkList.cpp
@@ -128,3 +128,18 @@ void searchData() {
 		cout << "Data tidak ditemukan" << endl;
 	}
 }
+
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "pilihan: ";
+			cin >> pilihan;
