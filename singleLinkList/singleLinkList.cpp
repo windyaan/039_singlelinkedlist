04950aee320fb221f5d614f45a2510ea757f44ac
windyaan <windyanurmaretha@gmail.com>
2023-05-18 21:51:27 +0700

    Method addNode

--- singleLinkList/singleLinkList.cpp
+++ singleLinkList/singleLinkList.cpp
@@ -9,3 +9,25 @@ struct Node {
 
 Node* START = NULL;
 
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
